b142b63794035570424e09628b9f547eada3ee02
unknown <med.harik@gmail.com>
2020-11-08 12:05:37 +0000

    VARIABLES & CAST

---
+++ projet_itsup_2021_licence/Base1.java
@@ -0,0 +1,39 @@
+package projet_itsup_2021_licence;
+
+public class Base1 {
+
+public static void main(String[] args) {
+	System.out.println("mon premier programme en java ");
+
+	
+	
+	//les variables : zones memoires reservees lors de la declaration
+	
+//	byte b=3;
+//	short s=10;
+//	int i=7000;
+//	long l=9999999;
+//	 b=(byte) i;//cast 
+//	System.out.println("b est "+b);
+	int i=3;
+	short s=10;
+	i=s;
+	s=(short)i;
+	System.out.println("s est "+s);
+	
+	
+	
+	
+	
+	/*
+	 test
+	 test 
+	 
+	 */
+	
+	
+	
+	
+	
+}	  
+}
